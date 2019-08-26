77767bbefd0920074819e4ae251bf9e6615e6a23
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 12:42:40 +0700

    add generator

--- blocks/blocks_mini-reed.js
+++ blocks/blocks_mini-reed.js
@@ -1,15 +1,18 @@
-Blockly.Blocks['ky_021_mini_reed_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-021 MINI REED");
-    this.appendValueInput("KY_021_MINI_REED_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_021_mini_reed_block"] = {
+    init: function() {
+      this.appendDummyInput()
+        .appendField("KY-021 MINI REED")
+        .appendField("PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "KY_021_MINI_REED_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("KY_021_MINI_REED_VALUE"),
+          "KY_021_MINI_REED_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_mini-reed.js
+++ blocks/generators_mini-reed.js
@@ -1,6 +1,20 @@
-Blockly.JavaScript['ky_021_mini_reed_block'] = function(block) {
-  var value_ky_021_mini_reed_pin = Blockly.JavaScript.valueToCode(block, 'KY_021_MINI_REED_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_021_mini_reed_block"] = function(block) {
+    var text_ky_021_mini_reed_pin = block.getFieldValue("KY_021_MINI_REED_PIN");
+    var variable_ky_021_mini_reed_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("KY_021_MINI_REED_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+	#VARIABLE
+	int KY_021_MINI_REED_PIN = ${text_ky_021_mini_reed_pin}; // magnetic senso rpin
+	#END
+
+	#SETUP
+	pinMode (KY_021_MINI_REED_PIN, INPUT);
+	#END
+
+	${variable_ky_021_mini_reed_value} = digitalRead(KY_021_MINI_REED_PIN); // read the value of digital interface 2 and assign it to switchState
+    `;
+    return code;
+  };
